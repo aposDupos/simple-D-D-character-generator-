ec7edd38e8eaf58e6ff63b7ea5920d84ee6662bd
Vlad Tyrtychnyi <tva100702@gmail.com>
2021-01-08 21:03:01 +0300

    some things fixed added validation on forms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, jsonify
+from flask import Flask, render_template, url_for, request, jsonify, redirect
 import json
 from static.script import randoming
 from pathlib import Path
@@ -12,7 +12,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/', methods=['post'])
+@app.route('/', methods=['post','get'])
 def add_data():
     race = request.form.get('select-race')
     sex = request.form.get('answer')
